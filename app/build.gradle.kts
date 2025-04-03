86becdfb9a4b861fafcd58c3a4c4ccf1f1eb2d7c
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2025-04-03 10:57:50 -0300

    chore: bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "ai.luxai.llmbench"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
